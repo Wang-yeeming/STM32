fd19b86422aa8a2bea2881dade481bfe9464c21b
Wang-yeeming <yeeming0771@foxmail.com>
2019-11-07 16:50:40 +0800

    hotfix

--- LED/main.c
+++ LED/main.c
@@ -1,5 +1,22 @@
-#include <stm32f10x.h>
+#include "stm32f10x.h"
+#include "led.h"
+
+void delay(__IO uint32_t uiCount){
+    for(; uiCount != 0; uiCount--);
+}
 
 void main(){
-    ;
+    LED_Init();
+    uint16_t x = GPIO_Pin_0;
+    
+    while(1){
+        GPIO_SetBits(GPIOB, GPIO_Pin_All);
+        GPIO_ResetBits(GPIOB, x);
+        x <<= 1;
+        delay(50);
+        
+        if(x == 0){
+            x = GPIO_Pin_0;
+        }
+    }
 }
